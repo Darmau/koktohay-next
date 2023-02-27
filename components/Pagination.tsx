32aafd478ad81e9cb406ae2207725b62360ef0a7
李大毛 <dev@darmau.design>
2023-02-27 11:19:42 +0800

    feat: pagination
    增加了在第一页和最后一页时，前一页或后一页禁用

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -67,8 +67,10 @@ const Pagination = ({
 
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <Link href="#"
-              className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
+            <Link href={`?page=${currentPage - 1}`}
+              className={currentPage === 1 
+              ? `relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-300 pointer-events-none	` 
+              : `relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20`}>
               <span className="sr-only">上一页</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </Link>
@@ -90,8 +92,10 @@ const Pagination = ({
               >...</div>
           }
             )}
-            <Link href="#"
-              className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
+            <Link href={`?page=${currentPage + 1}`}
+              className={currentPage === totalPages
+              ? `relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-300 pointer-events-none`
+              : `relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20`}>
               <span className="sr-only">下一页</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
             </Link>
