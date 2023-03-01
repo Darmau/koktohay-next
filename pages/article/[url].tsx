e3081c9f9d93aecfddb4081cb253af4c7316e396
李大毛 <dev@darmau.design>
2023-03-01 21:17:40 +0800

    feat: 增加目录
    以及其他样式调整

--- pages/article/[url].tsx
+++ pages/article/[url].tsx
@@ -2,18 +2,40 @@ import client from "@/apollo-client";
 import { gql } from "@apollo/client";
 import { ContentList } from "../function/Types";
 import Body from "@/components/Body";
+import Catalog from "@/components/Catalog";
+import Image from "next/image";
 
 export default function Article({ article }: any) {
-
   return (
-    <div className="bg-white py-32 px-6 lg:px-8">
-      <main className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        {/* 正文部分 */}
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{article.title}</h1>
-        <article className="text-base leading-7 text-gray-700">
+    <div className="bg-white py-32 px-6 max-w-5xl mx-auto  lg:grid lg:grid-cols-article lg:px-8 lg:gap-12">
+      <main className="w-full text-base leading-7 text-gray-700 lg:col-span-1">
+        {/* 封面和标题 */}
+        <header>
+          <p className="text-base font-semibold leading-7 text-indigo-600">
+            {article.article_category.data.attributes.title ?? '无分类'}
+          </p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {article.title}
+          </h1>
+          <Image
+            src={article.cover.data.attributes.url}
+            alt={article.title}
+            width={1280}
+            height={720}
+            className="rounded-lg bg-gray-50 object-cover my-4"
+          />
+        </header>
+
+        {/* 正文 */}
+        <div className="text-base leading-7 text-gray-700">
           <Body main={article.main} />
-        </article>
+        </div>
       </main>
+
+      {/* 目录 */}
+      <aside className="h-full lg:col-span-1">
+        <Catalog main={article.main} />
+      </aside>
     </div>
   );
 }
@@ -63,7 +85,7 @@ const GET_ALL_ARTICLE = gql`
     }
   }
 `;
-
+// 获取文章详细数据
 const GET_ARTICLE = gql`
   query Attributes($filters: ArticleFiltersInput, $locale: I18NLocaleCode) {
     articles(filters: $filters, locale: $locale) {
