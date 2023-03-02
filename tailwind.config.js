33216eec52ffcda6c734ce3868908a9e38d11f63
李大毛 <dev@darmau.design>
2023-03-02 19:56:45 +0800

    chore: remove package

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        'article': 'auto 256px',
-      }
+        article: "auto 256px",
+      },
     },
   },
   plugins: [
     // ...
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
