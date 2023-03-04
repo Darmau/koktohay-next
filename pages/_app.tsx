f24da1d76eb5eb68843b62d21a3f7fdd211f0d01
李大毛 <dev@darmau.design>
2023-03-04 21:12:15 +0800

    Add Analytics (#8)
    * fix: ISR
    
    * fix: remove ISR
    
    * fix: fix wrong path
    
    * chore: update package
    
    * refactor: remove page
    
    * fix: 英文地址错误的问题
    
    * perf: 优化点击体验
    
    * perf: 优化加载体验
    
    * perf: 优化相册表现
    
    * feat: 相册详情页
    
    支持切换及显示exif
    
    * feat: add language switch
    
    * chore: add package
    
    * remove unused import
    
    * feat: add map to album page
    
    * feat: add mapbox
    
    * feat: add word count to article
    
    * feat: 将oss域名替换为cdn域名
    
    * fix: 解决正文图片应用lightbox的useState错误
    
    * fix: 修复部署bug
    
    * perf: remove import
    
    将css移到_document
    
    * fix: i18n router bug
    
    * feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import Layout from '@/components/layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react'
-import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar'
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const loadingBarRef = useRef<LoadingBarRef>(null);
@@ -18,14 +19,14 @@ export default function App({ Component, pageProps }: AppProps) {
       loadingBarRef.current?.complete();
     };
 
-    router.events.on('routeChangeStart', start);
-    router.events.on('routeChangeComplete', stop);
-    router.events.on('routeChangeError', stop);
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeComplete", stop);
+    router.events.on("routeChangeError", stop);
 
     return () => {
-      router.events.off('routeChangeStart', start);
-      router.events.off('routeChangeComplete', stop);
-      router.events.off('routeChangeError', stop);
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", stop);
+      router.events.off("routeChangeError", stop);
     };
   }, [router.events]);
 
@@ -33,6 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Layout>
       <LoadingBar color="#f11946" ref={loadingBarRef} />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
-  )
+  );
 }
