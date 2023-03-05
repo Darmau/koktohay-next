8a48fc3cab04c6a7535c935f8d55473c4ddeaf25
李大毛 <dev@darmau.design>
2023-03-06 00:10:33 +0800

    feat: add video page

--- pages/video/[url].tsx
+++ pages/video/[url].tsx
@@ -1,15 +1,102 @@
 import client from "@/apollo-client";
+import ConvertToDate from "@/function/ConvertDate";
 import { gql } from "@apollo/client";
-import { ContentList } from "../../function/Types";
+import { CalendarIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import { ContentList, ContentsProps } from "../../function/Types";
+import Image from "next/image";
+import xiguaIcon from "@/public/img/video-xigua.svg";
+import bilibiliIcon from "@/public/img/video-bilibili.svg";
+import youtubeIcon from "@/public/img/video-youtube.svg";
 
-export default function Video({video}: any) {
-  return <div>{video.attributes.title}</div>;
+export default function Video({ video }: ContentsProps) {
+  const { xigua, bilibili, youtube } = video;
+  const videoSources = [
+    {
+      name: "西瓜视频",
+      value: xigua,
+      icon: <Image className="h-auto w-auto" src={xiguaIcon} alt="西瓜视频" width={136} height={72} />,
+    },
+    {
+      name: "YouTube",
+      value: youtube,
+      icon: <Image className="h-auto w-auto" src={youtubeIcon} alt="YouTube" width={136} height={72} />,
+    },
+    {
+      name: "哔哩哔哩",
+      value: bilibili,
+      icon: <Image className="h-auto w-auto" src={bilibiliIcon} alt="哔哩哔哩" width={136} height={72} />,
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  let frameKey = 0;
+
+  useEffect(() => {
+    const iframe = document.querySelector("#video-frame");
+    iframe!.setAttribute("src", videoSources[activeIndex].value);
+    frameKey++;
+  }, [activeIndex, videoSources]);
+
+  return (
+    <main className="mx-auto max-w-7xl pt-8 mb-8 sm:px-6 sm:pt-16 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:max-w-none">
+        <div className="lg:grid lg:grid-cols-3 lg:items-start lg:gap-x-8">
+          {/* 视频主体 */}
+          <div className="col-span-2 px-4">
+            <iframe
+              key={frameKey}
+              id="video-frame"
+              title={video.title}
+              className="w-full h-auto aspect-video"
+              src={videoSources[activeIndex].value}
+              allowFullScreen
+            ></iframe>
+            <div className="py-4 flex gap-4">
+              {videoSources.map(
+                (source, index) =>
+                  source.value && (
+                    <button
+                      key={source.name}
+                      className="border rounded-md px-2 py-1 border-gray-200 bg-white drop-shadow-sm transition-all hover:drop-shadow-md"
+                      onClick={() => setActiveIndex(index)}
+                    >
+                      {source.icon}
+                    </button>
+                  )
+              )}
+            </div>
+          </div>
+
+          {/* 视频信息 */}
+          <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:col-span-1 lg:mt-0">
+            <h1 className="text-3xl mb-4 font-bold tracking-tight text-gray-900">
+              {video.title}
+            </h1>
+
+            <div className="flex gap-1 items-center text-sm mb-4 text-gray-600">
+              <CalendarIcon className="h-4 w-4" />
+              {ConvertToDate(video.publishDate)}
+            </div>
+
+            <div className="mt-3">
+              <div className="mt-6">
+                <div className="space-y-6 text-base text-gray-700">
+                  {video.description}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export async function getStaticProps({ params, locale }: any) {
   const filters = {
     url: { eq: params.url },
-  }
+  };
   const { data } = await client.query({
     query: GET_VIDEO,
     variables: {
@@ -19,8 +106,9 @@ export async function getStaticProps({ params, locale }: any) {
   });
   return {
     props: {
-      video: data.videos.data[0],
+      video: data.videos.data[0].attributes,
     },
+    revalidate: 60,
   };
 }
 
@@ -29,10 +117,20 @@ export async function getStaticPaths() {
   const { data } = await client.query({
     query: GET_ALL_VIDEO,
   });
-  const paths = data.videos.data.flatMap((video: ContentList) => ([
-    { params: { url: video.attributes.url }, locale: "zh-CN"},
-    { params: { url: video.attributes.url }, locale: "en"},
-  ]));
+  const paths = data.videos.data.flatMap((video: ContentList) => {
+    const videoPaths = [
+      { params: { url: video.attributes.url }, locale: "zh-CN" },
+    ];
+
+    if (video.attributes.localizations?.data) {
+      videoPaths.push({
+        params: { url: video.attributes.url },
+        locale: "en",
+      });
+    }
+
+    return videoPaths;
+  });
   return {
     paths,
     fallback: false,
@@ -41,11 +139,16 @@ export async function getStaticPaths() {
 
 // 获取所有文章的url，用于生成静态页面
 const GET_ALL_VIDEO = gql`
-  query Videos($locale: I18NLocaleCode) {
-    videos(locale: $locale) {
+  query Videos {
+    videos {
       data {
         attributes {
           url
+          localizations {
+            data {
+              id
+            }
+          }
         }
       }
     }
@@ -58,6 +161,11 @@ const GET_VIDEO = gql`
       data {
         attributes {
           title
+          description
+          publishDate
+          xigua
+          bilibili
+          youtube
         }
       }
     }
--- pages/videos/[page].tsx
+++ pages/videos/[page].tsx
@@ -1,9 +1,13 @@
 import client from "@/apollo-client";
 import PageNotFound from "@/components/404";
 import Pagination from "@/components/Pagination";
-import { ContentList, ContentsProps } from "@/function/Types";
+import ConvertToDate from "@/function/ConvertDate";
+import getLabel from "@/function/GetLabel";
+import { ContentList, ContentsProps, Labels } from "@/function/Types";
 import { gql } from "@apollo/client";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function AllVideos({
@@ -17,24 +21,64 @@ export default function AllVideos({
     locale,
   } = router;
 
+  const label = getLabel(labels, locale);
+
   // 若页码超出范围，则返回404页面
   if (Number(page) > pagination!.pageCount) {
     return <PageNotFound />;
   }
 
   return (
-    <div className="h-full">
-      本页为{locale}页面，第{page}页
-      <ol>
-        {videos.map((item: ContentList, index: number) => (
-          <li key={index}>{item.attributes.title}</li>
-        ))}
-      </ol>
-      <Pagination
-        currentPage={Number(page)}
-        totalEntries={pagination!.total}
-        itemPerPage={pageSize}
-      />
+    <div className="bg-white py-8 sm:py-16">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl pt-4 pb-10 lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {label.header}
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">{label.slogan}</p>
+        </div>
+
+        {/* 视频主体区域 */}
+        <div className="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {videos.map((item: ContentList) => (
+            <article
+              key={item.id}
+              className="group relative isolate aspect-video w-full flex flex-col justify-end overflow-hidden cursor-pointer rounded-lg bg-gray-900 px-6 pb-6"
+            >
+              <Image
+                src={item.attributes.cover.data.attributes.url}
+                alt="cover"
+                className="absolute inset-0 -z-10 h-full w-full object-cover transition-all duration-300 group-hover:scale-105"
+                width={672}
+                height={378}
+              />
+              {/* 黑色遮罩 */}
+              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40 transition-all duration-300 group-hover:backdrop-blur-sm" />
+
+              <div className="flex flex-wrap items-center gap-y-1 text-sm leading-6 text-gray-300">
+                <time
+                  dateTime={item.attributes.publishDate!.toString()}
+                  className="mr-8"
+                >
+                  {ConvertToDate(item.attributes.publishDate)}
+                </time>
+              </div>
+              <h3 className="mt-2 text-xl font-normal leading-7 text-gray-200 transition-all group-hover:text-white">
+                <Link href={`/video/${item.attributes.url}`}>
+                  <span className="absolute inset-0" />
+                  {item.attributes.title}
+                </Link>
+              </h3>
+            </article>
+          ))}
+        </div>
+
+        <Pagination
+          currentPage={Number(page)}
+          totalEntries={pagination!.total}
+          itemPerPage={pageSize}
+        />
+      </div>
     </div>
   );
 }
@@ -45,11 +89,21 @@ const GET_VIDEOS = gql`
     $pagination: PaginationArg
     $locale: I18NLocaleCode
   ) {
-    videos(sort: $sort, pagination: $pagination, locale: $locale) {
+    videos(locale: $locale, sort: $sort, pagination: $pagination) {
       data {
         attributes {
           title
+          url
+          publishDate
+          cover {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
         }
+        id
       }
       meta {
         pagination {
@@ -68,7 +122,7 @@ export const getServerSideProps: GetServerSideProps<ContentsProps> = async (
   const { locale, query } = context;
   const { page } = query;
   const pagination = {
-    pageSize: 2,
+    pageSize: 12,
     page: Number(page),
   };
   const { data } = await client.query({
@@ -83,4 +137,15 @@ export const getServerSideProps: GetServerSideProps<ContentsProps> = async (
       pageSize: pagination.pageSize,
     },
   };
+};
+
+const labels: Labels = {
+  "zh-CN": {
+    header: "视频",
+    slogan: "我的原则是想怎么剪就怎么剪",
+  },
+  en: {
+    header: "Videos",
+    slogan: "My principle is to cut as I want",
+  },
 };
