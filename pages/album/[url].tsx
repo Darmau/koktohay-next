1917d5a806e4ca3fbb2dec3e0b6c725568d2be39
李大毛 <dev@darmau.design>
2023-03-09 23:50:46 +0800

    feat: 优化图片浏览

--- pages/album/[url].tsx
+++ pages/album/[url].tsx
@@ -1,64 +1,30 @@
 import client from "@/apollo-client";
+import Carousel from "@/components/Carousel";
 import { Comments } from "@/components/Comment";
-import ExifInfo from "@/components/ExifInfo";
 import MapWithExif from "@/components/Map";
 import ConvertToDate from "@/function/ConvertDate";
 import { gql } from "@apollo/client";
-import { Tab } from "@headlessui/react";
 import {
-  ArrowSmallLeftIcon,
-  ArrowSmallRightIcon,
-  ArrowsPointingOutIcon,
   CalendarIcon
 } from "@heroicons/react/20/solid";
 import { ArticleJsonLd, NextSeo } from "next-seo";
-import Image from "next/image";
-import { useState } from "react";
-import Lightbox from "yet-another-react-lightbox";
-import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/styles.css";
-import { ContentList, ContentsProps, PhotoArray } from "../../function/Types";
-import Inline from "yet-another-react-lightbox/plugins/inline";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import { ContentList, ContentsProps, Photos } from "../../function/Types";
 
 export default function Album({ album }: ContentsProps) {
-  // 根据返回的相册数据，生成一个更简洁结构的图片数组
-  const originalPhoto: PhotoArray = album.gallery.data.map(
-    (photo: ContentList) => {
-      return {
-        src: photo.attributes.url,
-        width: photo.attributes.width,
-        height: photo.attributes.height,
-        alt: album.title,
-      };
-    }
-  );
 
-  const largePhoto: PhotoArray = album.gallery.data.map((photo: ContentList) => {
+  const previewPhoto = album.gallery.data.map((photo: ContentList) => {
     return {
-      src: photo.attributes.formats.large.url,
-      width: photo.attributes.formats.large.width,
-      height: photo.attributes.formats.large.height,
+      src: photo.attributes.formats.small.url,
+      width: photo.attributes.formats.small.width,
+      height: photo.attributes.formats.small.height,
       alt: album.title,
+      large: photo.attributes.formats.large.url,
+      original: photo.attributes.url,
     }
   });
 
-  const smallPhoto: PhotoArray = album.gallery.data.map((photo: ContentList) => {
-    return {
-      src: photo.attributes.formats.thumbnail.url,
-      width: photo.attributes.formats.thumbnail.width,
-      height: photo.attributes.formats.thumbnail.height,
-      alt: album.title,
-    }
-  });
-
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [open, setOpen] = useState(false);
 
   return (
     <>
@@ -98,115 +64,11 @@ export default function Album({ album }: ContentsProps) {
         isAccessibleForFree={true}
       />
 
-      <Lightbox
-        open={open}
-        close={() => setOpen(false)}
-        slides={originalPhoto}
-        plugins={[Thumbnails, Zoom]}
-      />
-
       <main className="mx-auto max-w-7xl pt-8 mb-8 sm:px-6 sm:pt-16 lg:px-8">
         <div className="mx-auto max-w-2xl lg:max-w-none">
           <div className="lg:grid lg:grid-cols-3 lg:items-start lg:gap-x-8">
 
-            <Tab.Group
-              as="div"
-              className="flex flex-col-reverse col-span-2 px-4"
-              selectedIndex={selectedIndex}
-              onChange={(index) => setSelectedIndex(index as number)}
-            >
-
-              <div className="mx-auto mt-6 block py-4 w-full max-w-2xl lg:max-w-none">
-                <Tab.List className="grid grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-4">
-                  {smallPhoto.map((image, index: number) => (
-                    <Tab
-                      key={index}
-                      className="relative flex h-32 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
-                    >
-                      {({ selected }) => {
-                        const ratio = image.height / image.width;
-                        return (
-                          <>
-                            <span className="absolute inset-0 overflow-hidden rounded-md">
-                              <Image
-                                src={image.src}
-                                alt={image.alt}
-                                width={240}
-                                height={240 * ratio}
-                                priority
-                                quality={60}
-                                className="h-full w-full object-contain object-center"
-                              />
-                            </span>
-                            <span
-                              className={classNames(
-                                selected
-                                  ? "ring-blue-500"
-                                  : "ring-transparent",
-                                "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2"
-                              )}
-                              aria-hidden="true"
-                            />
-                          </>
-                        );
-                      }}
-                    </Tab>
-                  ))}
-                </Tab.List>
-              </div>
-
-              <Tab.Panels className="w-full relative">
-                {largePhoto.map((image, index: number) => {
-                  return (
-                    <Tab.Panel key={index}>
-                      {index > 0 && (
-                        <button
-                          type="button"
-                          className="absolute top-1/2 left-4 transform -translate-y-1/2 text-white bg-gray-900/20 backdrop-blur hover:bg-gray-900/60 rounded-full p-2"
-                          onClick={() => setSelectedIndex(index - 1)}
-                        >
-                          <ArrowSmallLeftIcon
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        </button>
-                      )}
-                      {index < originalPhoto.length - 1 && (
-                        <button
-                          type="button"
-                          className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white bg-gray-900/20 backdrop-blur hover:bg-gray-900/60 rounded-full p-2"
-                          onClick={() => setSelectedIndex(index + 1)}
-                        >
-                          <ArrowSmallRightIcon
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                          />
-                        </button>
-                      )}
-                      <button
-                        type="button"
-                        className="absolute top-8 right-4 transform -translate-y-1/2 text-white bg-gray-900/20 backdrop-blur hover:bg-gray-900/60 rounded-full p-2"
-                        onClick={() => setOpen(true)}
-                      >
-                        <ArrowsPointingOutIcon
-                          className="h-5 w-5"
-                          aria-hidden="true"
-                        />
-                      </button>
-                      <Image
-                        src={image.src}
-                        alt={image.alt}
-                        width={1280}
-                        height={720}
-                        className="w-full h-auto object-contain object-center sm:rounded-lg"
-                      />
-
-                      <ExifInfo url={originalPhoto[index].src} />
-                    </Tab.Panel>
-                  );
-                })}
-              </Tab.Panels>
-            </Tab.Group>
+            <Carousel photos={previewPhoto} />
 
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:col-span-1 lg:mt-0">
               <h1 className="text-3xl font-bold mb-4 tracking-tight text-gray-900">
@@ -226,7 +88,7 @@ export default function Album({ album }: ContentsProps) {
                 </div>
               </div>
               {/* 放置地图 */}
-              <MapWithExif src={originalPhoto[0].src} />
+              <MapWithExif src={previewPhoto[0].original} />
               <Comments location={""} />
             </div>
           </div>
